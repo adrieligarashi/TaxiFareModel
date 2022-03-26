c5df9ad592d0a43a6dced96d70908ccadb55455e
Adriel Igarashi <adriel.igarashi@gmail.com>
2022-03-26 10:16:51 -0300

    sabado inicial

--- TaxiFareModel/trainer copy.py
+++ TaxiFareModel/trainer copy.py
@@ -1,3 +1,4 @@
+from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
@@ -11,16 +12,6 @@ from mlflow.tracking import MlflowClient
 from memoized_property import memoized_property
 import joblib
 
-from sklearn.model_selection import cross_val_score
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.linear_model import Ridge, Lasso, LinearRegression
-from sklearn.svm import SVR
-from sklearn.ensemble import AdaBoostRegressor
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.ensemble import RandomForestRegressor
-from xgboost import XGBRegressor
-
 
 class Trainer():
     def __init__(self, X, y):
@@ -32,7 +23,7 @@ class Trainer():
         self.X = X
         self.y = y
 
-    def set_pipeline(self, model):
+    def set_pipeline(self):
         """defines the pipeline as a class attribute"""
 
         '''returns a pipelined model'''
@@ -48,12 +39,12 @@ class Trainer():
         ]), ('time', self.time_pipe, ['pickup_datetime'])],
                                               remainder="drop")
         self.pipe = Pipeline([('preproc', self.preproc_pipe),
-                              ('linear_model', model)])
+                              ('linear_model', LinearRegression())])
         return self.pipe
 
-    def run(self, model):
+    def run(self):
         """set and train the pipeline"""
-        self.set_pipeline(model)
+        self.set_pipeline()
         self.pipe.fit(self.X, self.y)
         return self.pipe
 
@@ -64,17 +55,6 @@ class Trainer():
         self.rmse = compute_rmse(self.y_pred, y_test)
         print(self.rmse)
         return self.rmse
-
-    def cvScore(self, X_test, y_test):
-        '''Cross Validate'''
-        self.cv = cross_val_score(
-            self.pipe,
-            X_test,
-            y_test,
-            cv=5,
-            scoring='neg_root_mean_squared_error').mean()
-        print(self.cv)
-        return self.cv
 
 
 
@@ -96,18 +76,18 @@ class Trainer():
             return self.client.get_experiment_by_name(
                 self.experiment_name).experiment_id
 
+    @memoized_property
     def mlflow_run(self):
         print(
             f"experiment URL: https://mlflow.lewagon.co/#/experiments/{self.experiment_id}"
         )
-        self.mlfow_running = self.client.create_run(self.mlflow_experiment_id)
-        return self.mlfow_running
+        return self.client.create_run(self.mlflow_experiment_id)
 
     def mlflow_log_param(self, key, value):
-        self.client.log_param(self.mlfow_running.info.run_id, key, value)
+        self.client.log_param(self.mlflow_run.info.run_id, key, value)
 
     def mlflow_log_metric(self, key, value):
-        self.client.log_metric(self.mlfow_running.info.run_id, key, value)
+        self.client.log_metric(self.mlflow_run.info.run_id, key, value)
 
 
     def save_model(self):
@@ -115,43 +95,19 @@ class Trainer():
 
 
 if __name__ == "__main__":
-    df = get_data(100000)
+    df = get_data()
     df = clean_data(df)
     y = df["fare_amount"]
     X = df.drop("fare_amount", axis=1)
-    # X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.15)
-    # pipeline = Trainer(X_train, y_train)
-    pipeline = Trainer(X, y)
-    models = [
-        LinearRegression(),
-        Lasso(),
-        Ridge(),
-        KNeighborsRegressor(),
-        SVR(kernel='linear'),
-        RandomForestRegressor(max_depth=50, min_samples_leaf=20),
-        AdaBoostRegressor(base_estimator=DecisionTreeRegressor(
-            max_depth=None)),
-        GradientBoostingRegressor(n_estimators=100, verbose=0),
-        XGBRegressor(max_depth=10, n_estimators=300, learning_rate=0.1)
-    ]
-    models_str = ['LinearRegression', 'Lasso', 'Ridge', 'KNeighborsRegressor', 'SVM', 'RandomForest', 'AdaBoostRegressor', 'GradientBoosting', 'XGBoost']
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.15)
+    pipeline = Trainer(X_train, y_train)
+    pipeline.run()
+    RMSE = pipeline.evaluate(X_val, y_val)
 
-    for key, model in enumerate(models):
-        print('modelo: ', model)
-        pipeline.run(model)
-        pipeline.cvScore(X, y)
-        pipeline.mlflow_client
-        pipeline.mlflow_experiment_id
-        pipeline.mlflow_run()
-        pipeline.mlflow_log_param('model ', model) #models_str[key])
-        pipeline.mlflow_log_metric('NRMSE ', pipeline.cv)
+    pipeline.mlflow_client
+    pipeline.mlflow_experiment_id
+    pipeline.mlflow_run
+    pipeline.mlflow_log_param('model', 'linear')
+    pipeline.mlflow_log_metric('RMSE', RMSE)
 
-    # RMSE = pipeline.evaluate(X_val, y_val)
-
-
-
-
-
-
-
-    #pipeline.save_model()
+    pipeline.save_model()
